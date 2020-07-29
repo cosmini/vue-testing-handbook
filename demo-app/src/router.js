deca727c7b954962e2e18681e14e1b66e0c83118
Cosmin Ianculescu <cosmin@visualdomain.com.au>
2020-07-29 12:42:52 +1000

    Added getter for authenticated state. Updated router nav guard to get authentication state from store getter.

--- demo-app/src/router.js
+++ demo-app/src/router.js
@@ -2,12 +2,15 @@ import Vue from "vue"
 import VueRouter from "vue-router"
 import routes from "./routes.js"
 import { bustCache } from "@/bust-cache.js"
+import store from './store'
 
 Vue.use(VueRouter)
 
-const router = new VueRouter({ routes })
+const router = new VueRouter({ routes, mode: 'history' })
 
 export function beforeEach(to, from, next) {
+  const isAuthenticated = store.getters['isAuthenticated']
+  console.log(isAuthenticated)
   if (to.matched.some(record => record.meta.shouldBustCache)) {
     bustCache()
   }
--- demo-app/src/store/getters.js
+++ demo-app/src/store/getters.js
@@ -1,4 +1,8 @@
 export default {
+  isAuthenticated: (state) => {
+    return state.authenticated
+  },
+
   poodles: (state) => {
     return state.dogs.filter(dog => dog.breed === "poodle")
   },
